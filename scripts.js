62507a3d344cf3b047f562b20d4e677d0ca74043
Spark Wu <wujiyu@wujiyu.com>
2024-03-30 11:08:30 +0800

    Optimize auto size

--- scripts.js
+++ scripts.js
@@ -179,12 +179,12 @@ function exportWinners() {
     for (eachWinner of winners) {     // DIV 中逐一插入中间人的名字
         const winnerBox = document.createElement('div');
         winnerBox.classList.add('winner-box');
-        let scaleFactor =  Math.sqrt(Math.sqrt(10 / winners.length));
+        let scaleFactor =  Math.sqrt(10 / winners.length);
         if (winners.length >= 10) {
-            winnerBox.style.fontSize = 60*scaleFactor*scaleFactor +'px'; 
-            winnerBox.style.margin = 20*scaleFactor*scaleFactor +'px'; 
-            winnerBox.style.minWidth = 280*scaleFactor*scaleFactor +'px'; 
-            winnerBox.style.borderRadius = 20*scaleFactor +'px'; 
+            winnerBox.style.fontSize = 60 * scaleFactor +'px'; 
+            winnerBox.style.margin = 20 * scaleFactor +'px'; 
+            winnerBox.style.minWidth = 280 * scaleFactor +'px'; 
+            winnerBox.style.borderRadius = 20 * Math.sqrt(scaleFactor) +'px'; 
         }                              //中奖人大于 10 的时候，自动无极计算方框大小
         else if (winners.length >= 5) {
             winnerBox.classList.add('winner-box-5');
