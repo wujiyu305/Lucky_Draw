433d3460bafec465d66dba7d3caa2c73a08d60d2
Spark Wu <wujiyu@wujiyu.com>
2024-04-02 17:13:01 +0800

    suppoort Chinese and English on one page, and support live language switch

--- scripts.js
+++ scripts.js
@@ -1,8 +1,22 @@
 let names = [];
 let winners = [];
 let interval;
-let speed = 150;     //默认滚动速度 150 ms
+let speed = 150;     //默认滚动刷新速度 150 ms
 let pngwaiting = 1;
+let lang = 'zh';
+let string_importAlert;
+let string_importAlert_oneName;
+let string_importAlert_importFail;
+let string_importAlert_type;
+let string_importAlert_typeFail;
+let string_nameAlert;
+let string_startAlert_nonInt;
+let string_winnerAlert_noName;
+let string_winnerAlert_biggerWinner;
+let string_winnerAlert_oneWinner;
+let string_exportAlert;
+let string_exportTemplate_p1;
+let string_exportTemplate_p2;
 listener();
 
 function listener() {
@@ -20,12 +34,12 @@ function listener() {
         document.getElementById('speed').value = urlPara.get('speed');
         
         /* 加载 String */
-        stringLoader();
+        stringLoader();        
 
         /* 网页加载完后 3 秒延迟隐藏说明窗口 */
-        setTimeout(function() {
+/*         setTimeout(function() {
             showInfo('hide');
-        }, 3000);
+        }, 3000); */
     });
     document.addEventListener('keydown', function(event){
         if(event.key ==' ') {       //监听空格键来触发开始抽奖
@@ -45,76 +59,99 @@ function listener() {
     });
 }
 
+function setLang(language) {
+    if (language == 'zh'){
+        lang = language;
+        stringLoader();
+        document.getElementById("btnChinese").disabled = true;
+        document.getElementById("btnEnglish").disabled = false;
+    }
+    else if (language == 'en'){
+        lang = language;
+        stringLoader();
+        document.getElementById("btnChinese").disabled = false;
+        document.getElementById("btnEnglish").disabled = true;
+    }
+}
+
 function stringLoader() {
 
     /* 多语言网页 String 设置区域 */
     /* zh */
-    const string_pageTitle_zh = "幸运抽奖";
-    const string_infoTitle_zh = "使用说明";
-    const string_infoContent_zh = "<li><p>首先请使用“设置名单”设置候选名单，支持导入 txt 文本文件，文件中可使用换行或者逗号来间隔多个候选人。若有重复名字，会自动去重。</p></li><li><p>成功名单成功后，名单按钮自动显示名单的总人数，点击按钮显示详细名单。</p></li><li><p>然后请设置本轮抽奖数量，请设置得低于总人数。</p></li><li><p>点击“开始”以开始抽奖，屏幕上会滚动随机显示对应数量的中奖人。点击“停止”以停止滚动，此时中奖人固定不再变化，视为中奖。</p></li><li><p>支持使用键盘上的空格键触发开始/停止。</p></li><li><p>使用“导出并移除”导出一个以当前时间为文件名的 txt 文件，包含当前中奖的名单和尚未中奖的名单，同时会将已经中奖的名单从总名单中移除，避免重复中奖。</p></li>";
-    const string_settingsTitle_zh = "设置";
-    const string_winnerSize_zh = "中奖人显示大小";
-    const string_winnerSpeed_zh = "中奖人滚动速度";
-    const string_expTXT_zh = "导出 txt 文本";
-    const string_expPNG_zh = "导出 png 文本";
-    const string_startBtn_zh = "开始";
-    const string_startBtn_title_zh = "设置名单后，点击开始按钮或者按下键盘空格键以开始抽奖，屏幕上会滚动随机显示对应数量的中奖人。";
-    const string_stopBtn_zh = "停止";
-    const string_stopBtn_title_zh = "点击停止按钮或者按下键盘空格键以停止，中奖人固定不再变化，视为中奖。";
-    const string_numInput_title_zh = "设置本轮抽奖的中奖人数量，请设置得低于总人数。";
-    const string_setBtn_zh = "设置名单";
-    const string_setBtn_title_zh = "支持导入 txt 纯文本文件，请在 txt 中通过换行或者逗号来间隔多个候选人名字，不要出现重复的名字。如果人数较少，也可以输入名单。";
-    const string_nameBtn_zh = "显示名单";
-    const string_nameBtn_title_zh = "显示当前奖池的名单。";;
-    const string_removeBtn_zh = "导出并移除";
-    const string_removeBtn_title_zh = "导出一个以当前时间为文件名的 txt 文档，其中包含当前中奖的名单和尚未中奖的名单，同时将已经中奖的名单从总名单中移除，避免重复中奖。";
- 
+    if (lang == 'zh') {
+        document.title = "幸运抽奖";
+        document.getElementById('infoBotton').title = "使用说明";
+        document.getElementById('infoTitle').innerText = "使用说明";
+        document.getElementById('infoContent').innerHTML = "<li><p>首先请使用“设置名单”设置候选名单，支持导入 txt 文本文件，文件中可使用换行或者逗号来间隔多个候选人。若有重复名字，会自动去重。</p></li><li><p>成功名单成功后，名单按钮自动显示名单的总人数，点击按钮显示详细名单。</p></li><li><p>然后请设置本轮抽奖数量，请设置得低于总人数。</p></li><li><p>点击“开始”以开始抽奖，屏幕上会滚动随机显示对应数量的中奖人。点击“停止”以停止滚动，此时中奖人固定不再变化，视为中奖。</p></li><li><p>支持使用键盘上的空格键触发开始/停止。</p></li><li><p>使用“导出并移除”导出一个以当前时间为文件名的 txt 文件，包含当前中奖的名单和尚未中奖的名单，同时会将已经中奖的名单从总名单中移除，避免重复中奖。</p></li>";
+        document.getElementById('settingsBotton').title = "设置";
+        document.getElementById('settingsTitle').innerText = "设置";
+        document.getElementById('winnerSize').innerText = "中奖人显示大小";
+        document.getElementById('winnerSpeed').innerText = "中奖人滚动速度";
+        document.getElementById('expTXT').innerText = "导出 txt 文本";
+        document.getElementById('expPNG').innerText = "导出 png 文件";
+        document.getElementById('startBtn').innerText = "开始";
+        document.getElementById('startBtn').title = "设置名单后，点击开始按钮或者按下键盘空格键以开始抽奖，屏幕上会滚动随机显示对应数量的中奖人。";
+        document.getElementById('stopBtn').innerText = "停止";
+        document.getElementById('stopBtn').title = "点击停止按钮或者按下键盘空格键以停止，中奖人固定不再变化，视为中奖。";
+        document.getElementById('numInput').title = "设置本轮抽奖的中奖人数量，请设置得低于总人数。";
+        document.getElementById('setBtn').innerText = "设置名单";
+        document.getElementById('setBtn').title = "支持导入 txt 纯文本文件，请在 txt 中通过换行或者逗号来间隔多个候选人名字，不要出现重复的名字。如果人数较少，也可以输入名单。";
+        document.getElementById('nameBtn').innerText = "显示名单";
+        document.getElementById('nameBtn').title = "显示当前奖池的名单。";
+        document.getElementById('removeBtn').innerText = "导出并移除";
+        document.getElementById('removeBtn').title = "导出一个以当前时间为文件名的 txt 文档，其中包含当前中奖的名单和尚未中奖的名单，同时将已经中奖的名单从总名单中移除，避免重复中奖。";
+        string_importAlert = "即将从 txt 文件导入名单。\n\n如果人数较少，可取消以手动输入名单。";
+        string_importAlert_oneName = "1 个人就不用抽奖来吧。";
+        string_importAlert_importFail = "导入失败，请检查 txt 文件。";
+        string_importAlert_type = "请输入抽奖名单，以逗号间隔多个名字。\n若有重复名字，将自动保留其中一个。";
+        string_importAlert_typeFail = "检测失败，请重新输入。";
+        string_nameAlert = "当前奖池中名单如下：\n\n";
+        string_startAlert_nonInt = "请确保在中奖人数处仅输入了整数哦。";
+        string_winnerAlert_noName = "抽奖名单现在是空的哦。";
+        string_winnerAlert_biggerWinner = "请设置比总人数少的中奖人数。";
+        string_winnerAlert_oneWinner = "请设置大于 1 的中奖人数。";
+        string_exportAlert = "这一轮还没有人中奖哦。";
+        string_exportTemplate_p1 = "********** 当轮中奖 **********\n\n";
+        string_exportTemplate_p2 = "\n\n****************************\n\n\n\n\n\n\n\n\n********** 尚未中奖 **********\n\n";
+    }
 
     /* en */
-    const string_pageTitle_en = "Lucky Draw";
-    const string_infoTitle_en = "Instruction";
-    const string_infoContent_en = "<li><p>Hit \"Set List\" to set candidates, you can import candidates from a txt file, seperate multiple names with comma or new line. Duplicated names would be auto-removed.</p></li><li><p>The number of total candidates would show on the button after list set.</p></li><li><p>Then please set the number of winners for this round.</p></li><li><p>Hit \"Start\", winner names would appear on screen and change quickly. Hit \"Stop\", name would stop changing and these names are winners.</p></li><li><p>You can use the Space key on keyboard to Start/Stop.</p></li><li><p>Use \"Exp & Rem\" (Export & Remove) to export a txt file names under current time, which contains winners for this round and non-winner names. And winners would be removed to prevent them from being winner again for next round.</p></li>";
-    const string_settingsTitle_en = "Settings";
-    const string_winnerSize_en = "中奖人显示大小";
-    const string_winnerSpeed_en = "中奖人滚动速度";
-    const string_expTXT_en = "导出 txt 文本";
-    const string_expPNG_en = "导出 png 文本";
-    const string_startBtn_en = "Start";
-    const string_startBtn_title_en = "设置名单后，点击开始按钮或者按下键盘空格键以开始抽奖，屏幕上会滚动随机显示对应数量的中奖人。";
-    const string_stopBtn_en = "Stop";
-    const string_stopBtn_title_en = "点击停止按钮或者按下键盘空格键以停止，中奖人固定不再变化，视为中奖。";;
-    const string_numInput_title_en = "设置本轮抽奖的中奖人数量，请设置得低于总人数。";
-    const string_setBtn_en = "Set List";
-    const string_setBtn_title_en = "支持导入 txt 纯文本文件，请在 txt 中通过换行或者逗号来间隔多个候选人名字，不要出现重复的名字。如果人数较少，也可以输入名单。";
-    const string_nameBtn_en = "Show Names";
-    const string_nameBtn_title_en = "显示当前奖池的名单。";;
-    const string_removeBtn_en = "Exp % Rem";
-    const string_removeBtn_title_en = "导出一个以当前时间为文件名的 txt 文档，其中包含当前中奖的名单和尚未中奖的名单，同时将已经中奖的名单从总名单中移除，避免重复中奖。";
- 
-    
-
-
-    document.title = string_pageTitle_zh;
-    document.getElementById('infoBotton').title = string_infoTitle_zh;
-    document.getElementById('infoTitle').innerText = string_infoTitle_zh;
-    document.getElementById('infoContent').innerHTML = string_infoContent_zh;
-    document.getElementById('settingsBotton').title = string_settingsTitle_zh;
-    document.getElementById('settingsTitle').innerText = string_settingsTitle_zh;
-    document.getElementById('winnerSize').innerText = string_winnerSize_zh;
-    document.getElementById('winnerSpeed').innerText = string_winnerSpeed_zh;
-    document.getElementById('expTXT').innerText = string_expTXT_zh;
-    document.getElementById('expPNG').innerText = string_expPNG_zh;
-    document.getElementById('startBtn').innerText = string_startBtn_zh;
-    document.getElementById('startBtn').title = string_startBtn_title_zh;
-    document.getElementById('stopBtn').innerText = string_stopBtn_zh;
-    document.getElementById('stopBtn').title = string_stopBtn_title_zh;
-    document.getElementById('numInput').title = string_numInput_title_zh;
-    document.getElementById('setBtn').innerText = string_setBtn_zh;
-    document.getElementById('setBtn').title = string_setBtn_title_zh;
-    document.getElementById('nameBtn').innerText = string_nameBtn_zh;
-    document.getElementById('nameBtn').title = string_nameBtn_title_zh;
-    document.getElementById('removeBtn').innerText = string_removeBtn_zh;
-    document.getElementById('removeBtn').title = string_removeBtn_title_zh;
+    else if (lang == 'en') {
+        document.title = "Lucky Draw";
+        document.getElementById('infoBotton').title = "Instruction";
+        document.getElementById('infoTitle').innerText = "Instruction";
+        document.getElementById('infoContent').innerHTML = "<li><p>Hit \"Set List\" to set candidates, you can import candidates from a txt file, seperate multiple names with comma or new line. Duplicated names would be auto-removed.</p></li><li><p>The number of total candidates would show on the button after list set.</p></li><li><p>Then please set the number of winners for this round.</p></li><li><p>Hit \"Start\", winner names would appear on screen and change quickly. Hit \"Stop\", name would stop changing and these names are winners.</p></li><li><p>You can use the Space key on keyboard to Start/Stop.</p></li><li><p>Use \"Exp & Rem\" (Export & Remove) to export a txt file names under current time, which contains winners for this round and non-winner names. And winners would be removed to prevent them from being winner again for next round.</p></li>";
+        document.getElementById('settingsBotton').title = "Settings";
+        document.getElementById('settingsTitle').innerText = "Settings";
+        document.getElementById('winnerSize').innerText = "Size of Winners";
+        document.getElementById('winnerSpeed').innerText = "Speed of Refresh";
+        document.getElementById('expTXT').innerText = "Export txt file";
+        document.getElementById('expPNG').innerText = "Export png file";
+        document.getElementById('startBtn').innerText = "Start";
+        document.getElementById('startBtn').title = "Hit \"Start\" or Space key, winner names would appear on screen and change quickly.";
+        document.getElementById('stopBtn').innerText = "Stop";
+        document.getElementById('stopBtn').title = "Hit \"Stop\" or Space key, name would stop changing and these names are winners.";;
+        document.getElementById('numInput').title = "Number of winners this this round. Should be less than number of total candidates.";
+        document.getElementById('setBtn').innerText = "Set List";
+        document.getElementById('setBtn').title = "Import candidate names from txt file, seperate miltiple names with comma or new line. You can also use type names for small list.";
+        document.getElementById('nameBtn').innerText = "Names";
+        document.getElementById('nameBtn').title = "Show all the candicates.";
+        document.getElementById('removeBtn').innerText = "Exp & Rem";
+        document.getElementById('removeBtn').title = "Export a txt file names under current time, which contains winners for this round and non-winner names. And winners would be removed to prevent them from being winner again for next round.";
+        string_importAlert = "Would import candidates from a txt file.\n\nIf you just got a small list, you can just cancel to type in candidates.";
+        string_importAlert_oneName = "You don't need a luck draw if you got only 1 candidate.";
+        string_importAlert_importFail = "Failed to set candidates from txt, please check the file.";
+        string_importAlert_type = "Please input candidate names.\nSeperated multiple names with comma.\nDuplicated names would be auto-removed.";
+        string_importAlert_typeFail = "Failed to detect candidates, please check and try again.";
+        string_nameAlert = "Below are candidates for now:\n\n";
+        string_startAlert_nonInt = "Please make sure to enter only integers for the number of winners.";
+        string_winnerAlert_noName = "Candidate list is still empty.";
+        string_winnerAlert_biggerWinner = "Winners should be less than candidates.";
+        string_winnerAlert_oneWinner = "Please allow at least 1 winner";
+        string_exportAlert = "There is no winner for this round yet.";
+        string_exportTemplate_p1 = "********** Winners for this round **********\n\n";
+        string_exportTemplate_p2 = "\n\n****************************\n\n\n\n\n\n\n\n\n********** Candidates left **********\n\n";
+    }
 }
 
 
@@ -141,8 +178,9 @@ function showSettings(showorhide) {
     info.style.transform = 'scale(0)';
 }
 
-function setNames() {    //导入名单
-    const choice = confirm("即将从 txt 文件导入名单。\n\n如果人数较少，可取消以手动输入名单。");
+function setNames() {    //设置名单
+    const choice = confirm(string_importAlert);
+
     if (choice) {
         const fileInput = document.getElementById('fileInput');
         fileInput.addEventListener('change', function(event) {
@@ -158,10 +196,10 @@ function setNames() {    //导入名单
                     document.getElementById('nameBtn').style.display = "";
                 }
                 else if (names.length == 1) {
-                    alert("1 个人就不用抽奖来吧。");
+                    alert(string_importAlert_oneName);
                 }
                 else {
-                    alert("导入失败，请检查 txt 文件。");
+                    alert(string_importAlert_importFail);
                 }
             };
         reader.readAsText(file);
@@ -169,7 +207,7 @@ function setNames() {    //导入名单
     fileInput.click();
     }
     else {
-        const contents = prompt('请输入抽奖名单，以逗号间隔多个名字。\n若有重复名字，将自动保留其中一个。');
+        const contents = prompt(string_importAlert_type);
         names = contents.split(/[,|，|\n]/).map(name => name.trim()).filter(name => name !== '');    //支持中英文逗号及换行间隔
         names = Array.from(new Set(names));    //去除重复值
         if (names.length > 1) {
@@ -178,17 +216,17 @@ function setNames() {    //导入名单
             document.getElementById('nameBtn').style.display = "";
         }
         else if (names.length == 1) {
-            alert("1 个人就不用抽奖来吧。");
+            alert(string_importAlert_oneName);
         }
         else {
-            alert("检测失败，请重新输入。");
+            alert(string_importAlert_typeFail);
         }
     }
 }
 
 function showNames() {
     const allNames = names.join('\n');
-    alert('当前奖池中名单如下：\n\n' +allNames);
+    alert(string_nameAlert +allNames);
 }
 
 function startDrawing() {    //开始抽奖
@@ -196,25 +234,25 @@ function startDrawing() {    //开始抽奖
     const numWinners = parseInt(numInput.value);    //获取设置的中奖人数并 Int 化
 
     if (numWinners != numInput.value){    //输入的中奖人数与 Int 化后的人数不一致，说明输入的不是整数
-        alert('请确保在中奖人数处仅输入了整数哦。');
+        alert(string_startAlert_nonInt);
         showInfo('show');
         return;
     }
     
     if (names.length == 0) {    //尚未设置名单的时候开始抽奖，则显示网页说明。
-        alert("抽奖名单现在是空的哦。");
+        alert(string_winnerAlert_noName);
         showInfo('show');
         return;
     }
     
     if (numWinners >= names.length) {    //中奖人数比总人数高或相同
-        alert('请设置比总人数少的中奖人数。');
+        alert(string_winnerAlert_biggerWinner);
         showInfo('show');
         return;
     }
     
     if (numWinners <= 0) {    //中奖人数不是正数
-        alert('请设置大于 1 的中奖人数。');
+        alert(string_winnerAlert_oneWinner);
         showInfo('show');
         return;
     }
@@ -317,7 +355,7 @@ function removeWinners() {    //移除中奖
 
 function exportWinners() {
     if (winners.length == 0) {     //还没人中奖时
-        alert("这一轮还没有人中奖哦。");
+        alert(string_exportAlert);
         return;
     }
 
@@ -354,7 +392,7 @@ function exportWinners() {
     if (document.getElementById('txt').checked) {
         const winnersContent = winners.join('\n');  // 中奖人名单
         const leftPoolContent = names.filter(name => !winners.includes(name)).join('\n'); // 尚未中奖人名单
-        const combinedContent = '********** 当轮中奖 **********\n\n' + winnersContent + '\n\n****************************' +'\n\n\n\n\n\n\n\n\n********** 尚未中奖 **********\n\n' + leftPoolContent;  // 整合中奖人名单和尚未中奖人的名单
+        const combinedContent = string_exportTemplate_p1 + winnersContent + string_exportTemplate_p2 + leftPoolContent;  // 整合中奖人名单和尚未中奖人的名单
 
         const filename = timestamp + '.txt';  // 使用当前日期时间作为名单 txt 文件名
         const element = document.createElement('a');
